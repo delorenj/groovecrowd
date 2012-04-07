2e368fad0ebb1241e1b6178794f2fd9f9384f922
Jarad DeLorenzo <jaradd@gmail.com>
2012-04-07 18:13:21 -0400

    Split into 3-tiered layout. Added better copy. Added a sweet footer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new GC\WelcomeBundle\GCWelcomeBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
