154084ca6113177a901b45b2781b02a249a7e595
Jarad DeLorenzo <jaradd@gmail.com>
2012-04-21 14:08:41 -0400

    Implmemneted hierarchical tree structure for categories in fixtures

--- src/GC/DataLayerBundle/DataFixtures/ORM/LoadSimpleData.php
+++ src/GC/DataLayerBundle/DataFixtures/ORM/LoadSimpleData.php
@@ -20,8 +20,37 @@ class LoadSimpleData extends AbstractFixture implements FixtureInterface, Ordere
     {
         $this->manager = $manager;
 
-        $app = $this->add("Application");
-        $this->add("Game", $app->getId());
+        $root = $this->add("root");
+        $game = $this->add("Game", $root->getId());
+        $this->add("Console", $game->getId());
+        $this->add("PC", $game->getId());
+        $this->add("Mobile", $game->getId());
+
+        $this->add("Brand Jingle", $root->getId());
+
+        $video = $this->add("Video", $root->getId());
+        $this->add("Advertisement", $video->getId());
+        $this->add("Web Series", $video->getId());
+        $anim = $this->add("Animation", $video->getId());
+
+        $root->setLft(1);
+        $rgt = $this->rebuild_tree($root);
+        $root->setRgt($rgt);
+        $this->manager->persist($root);
+        $this->manager->flush();
+    }
+
+    private function rebuild_tree($root, $lft=0) {
+        $children = $this->manager->getRepository('GC\DataLayerBundle\Entity\Category')->findBy(array('parentId' => $root->getId()));
+        foreach($children as $root) {
+            $root->setLft($lft+1);
+            $rgt = $this->rebuild_tree($root, $lft+1);
+            $root->setRgt($rgt);
+            $this->manager->persist($root);
+            $this->manager->flush();            
+            $lft=$rgt;          
+        }
+        return $lft;
     }
 
     private function add($name, $parent = NULL) {
