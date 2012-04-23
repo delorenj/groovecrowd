e7b370f8ea0d92388956faf69bd533749779ad7e
Jarad DeLorenzo <jaradd@gmail.com>
2012-04-23 19:21:38 -0400

    Added consumer and creator templates. Styling the navbar

--- src/GC/DashboardBundle/Controller/DefaultController.php
+++ src/GC/DashboardBundle/Controller/DefaultController.php
@@ -3,7 +3,8 @@
 namespace GC\DashboardBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
@@ -12,8 +13,27 @@ class DefaultController extends Controller
     {
     	$user = $this->get('security.context')->getToken()->getUser();
     	$userRepo = $this->userRepo = $this->getDoctrine()->getRepository('GCDataLayerBundle:User');
-    	$projects = $userRepo->findAllActiveProjects($user);
-        return $this->render('GCDashboardBundle:Default:index.html.twig', 
-        	array('projects' => $projects));
+    	if($user->hasRole('ROLE_CREATOR')) {
+    		return $this->forward('GCDashboardBundle:Default:creatorIndex');
+    		return $r;
+    	} elseif($user->hasRole('ROLE_CONSUMER')) {
+    		return $this->forward('GCDashboardBundle:Default:consumerIndex');
+    	}
     }
+
+    public function consumerIndexAction() {
+    	$user = $this->get('security.context')->getToken()->getUser();
+    	$userRepo = $this->userRepo = $this->getDoctrine()->getRepository('GCDataLayerBundle:User');    	
+    	$projects = $userRepo->findAllActiveProjects($user);
+        return $this->render('GCDashboardBundle:Default:consumer.html.twig', 
+        	array('projects' => $projects));
+	}
+
+    public function creatorIndexAction() {
+    	$user = $this->get('security.context')->getToken()->getUser();
+    	$userRepo = $this->userRepo = $this->getDoctrine()->getRepository('GCDataLayerBundle:User');    	
+    	$grooveSets = $user->getGrooveSets();
+        return $this->render('GCDashboardBundle:Default:creator.html.twig', 
+        	array('grooveSets' => $grooveSets));
+	}	
 }
