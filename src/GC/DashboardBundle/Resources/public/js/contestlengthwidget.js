2ea3eca84058d76e47ef1910704a3eff4bb6f472
Jarad DeLorenzo <jaradd@gmail.com>
2012-06-17 17:26:13 -0400

    Added arrows to the contestLengthWidget

--- src/GC/DashboardBundle/Resources/public/js/contestlengthwidget.js
+++ src/GC/DashboardBundle/Resources/public/js/contestlengthwidget.js
@@ -1,14 +1,45 @@
-$(document).ready(function() {    
-    var countdown = $("#contestLengthWidgetCountdown");
+(function(ContestLengthWidget, $) {
     var widget = $("#contestLengthWidget .bar");
+    var arrowContainer = $("#contestLengthWidgetContainer");
+    var arrow = "<div class='contestLengthWidgetArrow'><h5 style='width: 60px;'>Day 1</h5><img src='/img/arrow-blue-outline-down-23x30.png'></img></div>"
+    var countdown = $("#contestLengthWidgetCountdown");    
     var expiresAt = $(countdown).attr("data-time");
-    $(widget).width($(widget).attr('data-fill'));    
-    setInterval(
-        function() {
-            $(countdown).html(
-                    remaining.getString(remaining.getSeconds(expiresAt), null, false) + ' left!'
-                );
+    var larrow = $(arrow);
+    var rarrow = $(arrow);
 
-        }, 1000
-    );
+    ContestLengthWidget.init = function() {
+        initProgressBar();
+        initCountdownTimer();
+    }
+
+    function initProgressBar() {
+        complete = $(widget).attr('data-fill');
+        setPercentComplete(larrow, complete);
+        setPercentComplete(rarrow, 100);        
+        $(widget).width(complete);
+        $(arrowContainer).prepend(rarrow);
+        $(arrowContainer).prepend(larrow);
+    }
+
+    function initCountdownTimer() {
+        setInterval(
+            function() {
+                $(countdown).html(remaining.getString(remaining.getSeconds(expiresAt), null, false) + ' left!');
+            }, 1000
+        );
+    }
+
+    function setPercentComplete(arrow, val) {
+        $(arrow).css("left", val + "%");
+    }
+
+    function getPercentageFromRemainingTime(secondsLeft) {
+
+    }
+
+})(window.App.ContestLengthWidget = window.App.ContestLengthWidget || {}, jQuery);
+
+
+$(document).ready(function() {    
+    App.ContestLengthWidget.init();
 });
