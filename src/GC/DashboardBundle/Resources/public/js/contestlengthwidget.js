d8daddc4b8fa58978bca4ba814c4c7e32c406517
Jarad DeLorenzo <jaradd@gmail.com>
2012-06-19 19:36:52 -0400

    Awesome contest widgetness

--- src/GC/DashboardBundle/Resources/public/js/contestlengthwidget.js
+++ src/GC/DashboardBundle/Resources/public/js/contestlengthwidget.js
@@ -1,25 +1,29 @@
 (function(ContestLengthWidget, $) {
     var widget = $("#contestLengthWidget .bar");
     var arrowContainer = $("#contestLengthWidgetContainer");
-    var arrow = Handlebars.compile($("#contestLengthWidgetArrowTemplate").html());
-    console.log(arrow);
+    var arrow = Handlebars.compile($("#arrow-template").html());
     var countdown = $("#contestLengthWidgetCountdown");    
     var expiresAt = $(countdown).attr("data-time");
-    var larrow = arrow({label: "Day 1"});
-    var rarrow = arrow({label: "Day 15"});
+    var larrow = arrow({id: "larrow", label: "Day 1", offset: 5});
+    var rarrow = arrow({id: "rarrow", label: "Day 15", offset: 9});
     
     ContestLengthWidget.init = function() {
         initProgressBar();
-        initCountdownTimer();
+        if(remaining.getSeconds(expiresAt) < 86400) {
+            initCountdownTimer();
+        } else {
+            date = expiresAt;
+            $(countdown).html("Contest ends on " + date);
+        }
     }
 
     function initProgressBar() {
         complete = $(widget).attr('data-fill');
-        setPercentComplete(larrow, complete);
-        setPercentComplete(rarrow, 100);        
-        $(widget).width(complete);
+        $(widget).width(complete + "%");
         $(arrowContainer).prepend(rarrow);
         $(arrowContainer).prepend(larrow);
+        setPercentComplete("larrow", complete);
+        setPercentComplete("rarrow", 100);        
     }
 
     function initCountdownTimer() {
@@ -31,7 +35,7 @@
     }
 
     function setPercentComplete(arrow, val) {
-        $(arrow).css("left", val + "%");
+        $("#" + arrow).css("left", val + "%");
     }
 
     function getPercentageFromRemainingTime(secondsLeft) {
