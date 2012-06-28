205ab36a5426cccc00405f1db2224895956f83c9
Jarad DeLorenzo <jaradd@gmail.com>
2012-06-28 19:02:00 -0400

    Bound comments to user ACL

--- src/GC/DataLayerBundle/DataFixtures/ORM/LoadProjectCommentData.php
+++ src/GC/DataLayerBundle/DataFixtures/ORM/LoadProjectCommentData.php
@@ -6,16 +6,25 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Security\Acl\Permission\MaskBuilder;	
 use GC\DataLayerBundle\Entity\ProjectComment;
 
 
-class LoadProjectCommentData extends AbstractFixture implements FixtureInterface, OrderedFixtureInterface
+class LoadProjectCommentData extends AbstractFixture implements FixtureInterface, OrderedFixtureInterface, ContainerAwareInterface
 {
+    protected $manager;
+    protected $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     public function getOrder() {
         return 25;
     }
-
-    protected $manager;
 
     public function load(ObjectManager $manager)
     {
@@ -29,6 +38,8 @@ class LoadProjectCommentData extends AbstractFixture implements FixtureInterface
         $p->setBody("Did you want something orchestral, or were you looking for something more electronic?");
         $this->manager->persist($p);
         $this->manager->flush();
+        $this->container->get('acl_helper')->bindUserToObject($p, MaskBuilder::MASK_OPERATOR, $manager->merge($this->getReference('user-creator')));
+
 
         $p = new ProjectComment();
         $p->setUser($manager->merge($this->getReference('user-consumer')));
@@ -38,6 +49,8 @@ class LoadProjectCommentData extends AbstractFixture implements FixtureInterface
         $p->setBody("I'm picturing something with strings. Maybe a pizzacato string section. I'm not sure - surprised me lol");
         $this->manager->persist($p);
         $this->manager->flush();
+        $this->container->get('acl_helper')->bindUserToObject($p, MaskBuilder::MASK_OPERATOR, $manager->merge($this->getReference('user-consumer')));
+
 
         $p = new ProjectComment();
         $p->setUser($manager->merge($this->getReference('test1')));
@@ -47,6 +60,6 @@ class LoadProjectCommentData extends AbstractFixture implements FixtureInterface
         $p->setBody("Can you add a few more examples to the inspriation board?");
         $this->manager->persist($p);
         $this->manager->flush();
-
+        $this->container->get('acl_helper')->bindUserToObject($p, MaskBuilder::MASK_OPERATOR, $manager->merge($this->getReference('test1')));
 	}
 }
