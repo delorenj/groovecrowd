f12d08b5858ec86e004e64276e2a65a4040465e6
Jarad DeLorenzo <jaradd@gmail.com>
2012-07-03 12:29:42 -0400

    moved js to global asset dir

---
+++ grunt.js
@@ -0,0 +1,57 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    meta: {
+      version: '0.1.0',
+      banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '* http://PROJECT_WEBSITE/\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+        'YOUR_NAME; Licensed MIT */'
+    },
+    lint: {
+      files: ['grunt.js', 'assets/js/**/*.js'] 
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:src/FILE_NAME.js>'],
+        dest: 'dist/FILE_NAME.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/FILE_NAME.min.js'
+      }
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint concat min');
+
+};
